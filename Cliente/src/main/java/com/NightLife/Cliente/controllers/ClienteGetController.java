d8fac30d183a0b396dda6bd321b11880fcd2851a
magobasti <82623546+Adrian-Bastidas@users.noreply.github.com>
2023-03-08 19:31:05 -0500

    Obtener todo el cliente

--- Cliente/src/main/java/com/NightLife/Cliente/controllers/ClienteGetController.java
+++ Cliente/src/main/java/com/NightLife/Cliente/controllers/ClienteGetController.java
@@ -1,11 +1,22 @@
 package com.NightLife.Cliente.controllers;
 
+import com.NightLife.Cliente.Entity.Cliente;
+import com.NightLife.Cliente.services.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/get")
 @CrossOrigin(value="*")
 public class ClienteGetController {
+@Autowired
+    private ClienteService servicio;
+public ResponseEntity<List<Cliente>> listar(){
+    return ResponseEntity.ok(servicio.listar());
+}
 }
